529c6421625e7c18cb74ef58b914016fd18db671
fleury <fleurytaty242@gmail.com>
2026-05-26 23:36:35 +0100

    projet_poo refactor: Todo list v4.0

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -12,32 +12,41 @@ class Todo {
 class TodoList {
   #tableauTache = [];
 
+  constructor() {
+    // Dès qu'on crée la liste, on tente de charger le localStorage
+    const donneesSauvegardees = localStorage.getItem("liste");
+    if (donneesSauvegardees) {
+      this.#tableauTache = JSON.parse(donneesSauvegardees);
+    }
+  }
+
   get lesTaches() {
     return this.#tableauTache;
   }
 
   ajouterTache(tache) {
     this.#tableauTache.push(tache);
+    this.#sauvegarderListe();
   }
-  sauvegarderListe() {
-    localStorage.setItem("liste", JSON.stringify(this.lesTaches));
+
+  #sauvegarderListe() {
+    localStorage.setItem("liste", JSON.stringify(this.#tableauTache));
   }
 }
 
 const boutonAjouter = document.getElementById("ajouter");
-const tableauDeTaches = new TodoList();
 const input = document.getElementById("input");
 const liste = document.querySelector(".liste-taches");
 
-function chargerListe() {
-  listeCharger = localStorage.getItem("liste");
-  if (listeCharger != null) {
-    const data = JSON.parse(listeCharger);
-    return data;
-  } else {
-    return console.log("Il n'y a aucune tâche dans le repertoire");
-  }
-}
+// function chargerListe() {
+//   listeCharger = localStorage.getItem("liste");
+//   if (listeCharger != null) {
+//     const data = JSON.parse(listeCharger);
+//     return data;
+//   } else {
+//     return console.log("Il n'y a aucune tâche dans le repertoire");
+//   }
+// }
 
 boutonAjouter.addEventListener("click", (e) => {
   if (input.value == "") {
@@ -45,8 +54,8 @@ boutonAjouter.addEventListener("click", (e) => {
     return console.error("Vous n'avez pas donné un nom pour votre tache");
   }
 
+  const tableauDeTaches = new TodoList();
   tableauDeTaches.ajouterTache(new Todo(Date.now(), input.value, false));
-  tableauDeTaches.sauvegarderListe();
 
   input.value = "";
   renderTache();
@@ -54,12 +63,16 @@ boutonAjouter.addEventListener("click", (e) => {
 
 function renderTache() {
   liste.innerHTML = "";
-  const taches = chargerListe();
+  const tableauDeTaches = new TodoList();
 
-  taches.forEach((tache) => {
+  tableauDeTaches.lesTaches.forEach((tache) => {
     const li = document.createElement("li");
     li.innerHTML = `<input type="checkbox"> <p>${tache.texte}</p> <button>Supprimer</button>`;
 
     liste.appendChild(li);
   });
+}
+
+window.onload = () =>{
+  renderTache();
 }
