7837f44a126edb439af98594ea62131c3dfde4a4
fleury <fleurytaty242@gmail.com>
2026-05-26 22:33:36 +0100

    projet_poo stockage-local + refactor: Todo list v3.0

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -19,6 +19,9 @@ class TodoList {
   ajouterTache(tache) {
     this.#tableauTache.push(tache);
   }
+  sauvegarderListe() {
+    localStorage.setItem("liste", JSON.stringify(this.lesTaches));
+  }
 }
 
 const boutonAjouter = document.getElementById("ajouter");
@@ -26,20 +29,34 @@ const tableauDeTaches = new TodoList();
 const input = document.getElementById("input");
 const liste = document.querySelector(".liste-taches");
 
-boutonAjouter.onclick = (e) => {
+function chargerListe() {
+  listeCharger = localStorage.getItem("liste");
+  if (listeCharger != null) {
+    const data = JSON.parse(listeCharger);
+    return data;
+  } else {
+    return console.log("Il n'y a aucune tâche dans le repertoire");
+  }
+}
+
+boutonAjouter.addEventListener("click", (e) => {
   if (input.value == "") {
     e.preventDefault();
     return console.error("Vous n'avez pas donné un nom pour votre tache");
   }
+
   tableauDeTaches.ajouterTache(new Todo(Date.now(), input.value, false));
-  console.log(tableauDeTaches.lesTaches);
+  tableauDeTaches.sauvegarderListe();
+
   input.value = "";
   renderTache();
-};
+});
 
 function renderTache() {
-  liste.innerHTML = "";  
-  tableauDeTaches.lesTaches.forEach((tache) => {
+  liste.innerHTML = "";
+  const taches = chargerListe();
+
+  taches.forEach((tache) => {
     const li = document.createElement("li");
     li.innerHTML = `<input type="checkbox"> <p>${tache.texte}</p> <button>Supprimer</button>`;
 
