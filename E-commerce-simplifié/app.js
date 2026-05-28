fd1cc0e557cbe794113905bc2a2766c16287375f
fleury <fleurytaty242@gmail.com>
2026-05-28 03:31:36 +0100

    projet_poo feat: e-commerce - v5.0

--- E-commerce-simplifié/app.js
+++ E-commerce-simplifié/app.js
@@ -19,12 +19,12 @@ class Panier {
 
 const monPanier = new Panier();
 monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/1.jpg"));
-monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/2.jpg"));
+monPanier.ajouterProduit(new Produit("Air Jordan 4", 3000, "images/2.jpg"));
 monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/3.jpg"));
 monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/1.jpg"));
-monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/2.jpg"));
+monPanier.ajouterProduit(new Produit("Air Jordan 4", 7500, "images/2.jpg"));
 monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/3.jpg"));
-monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/1.jpg"));
+monPanier.ajouterProduit(new Produit("Air Jordan 4", 4000, "images/1.jpg"));
 monPanier.ajouterProduit(new Produit("Air Jordan 4", 10000, "images/2.jpg"));
 
 function initialiserCatalogue(mesProduits) {
@@ -70,13 +70,11 @@ ListeProduits.addEventListener("click", (event) => {
   if (event.target.dataset.add == "ajouter") {
     const elementEnfant = event.target.parentNode.childNodes;
 
-    const [image, text] = elementEnfant;
+    const [image, text, ...rest] = elementEnfant;
 
     const childNodesText = text.childNodes;
     const [first, second] = childNodesText;
-    console.log(first.innerText);
-    console.log(second.innerText);
- 
+
     const li = document.createElement("li");
 
     const img = document.createElement("img");
@@ -86,16 +84,49 @@ ListeProduits.addEventListener("click", (event) => {
     nom.textContent = first.innerText;
 
     const prix = document.createElement("p");
-    nom.textContent = second.innerText;
+    prix.id = "price";
+    prix.textContent = parseInt(second.innerText);
+
+    const devise = document.createElement("span");
+    devise.textContent = "FCFA";
+
+    const contentPriceProduit = document.createElement("div");
+    contentPriceProduit.id = "contentPriceProduit";
+    contentPriceProduit.append(prix);
+    contentPriceProduit.append(devise);
 
     li.append(img);
     li.append(nom);
-    li.append(prix);
+    li.append(contentPriceProduit);
     listePanier.append(li);
+
+    afficherMontant();
   }
 
   const nombre = document.getElementById("nombre");
-  console.log(nombre);
   nombre.textContent = listePanier.childElementCount;
-  
 });
+
+function afficherMontant() {
+  const listePanier = document.getElementById("liste-panier");
+  const montant = document.getElementById("montant");
+
+  if (listePanier.childElementCount == 1) {
+    const prixProduit = document.getElementById("price");
+    montant.textContent = parseInt(prixProduit.innerText) + " FCFA";
+  } else if (listePanier.childElementCount >= 2) {
+    const prixParse = [];
+    const prixProduit = document.querySelectorAll(`#price`);
+    console.log(prixProduit);
+
+    prixProduit.forEach((prix) => {
+      prixParse.push(Number(prix.innerText));
+    });
+    console.log(prixParse);
+
+    const prixAcc = prixParse.reduce((acc, curr) => acc + curr, 0);
+    console.log(prixAcc);
+
+    montant.textContent = prixAcc + " FCFA";
+  }
+}
